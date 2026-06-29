f4543968086ea43d8b016d2eb116427dcf0b5094
rezi <emailko@gmail.com>
2026-06-29 14:23:26 +0700

    masuk

--- config/cors.php
+++ config/cors.php
@@ -6,10 +6,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://localhost:3000',
-    ],
+   'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+
 
     'allowed_origins_patterns' => [],
 
